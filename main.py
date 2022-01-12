6e336810a624d368c364130cad339e9d37b8469c
joel.neto <joel@LP1432.inmetrics.local>
2022-01-12 14:19:59 -0300

    Adicionado data de postagem e data de previsao de entrega

--- main.py
+++ main.py
@@ -10,20 +10,45 @@ codigos = ['OQ366239704BR']#,'OS474220058BR']
 clearConsole()
 welcome()
 
-print(f'Foi/foram encontrados {len(codigos)} objetos!')
+if(len(codigos) == 1):
+    print(f'Foi encontrado um objeto:')
+else:
+    print(f'Foram encontrados {len(codigos)} objetos:')
 
 for i in codigos:
     try:
 
         req = requests.post(url=url, data={"objetos": i})
         soup = BeautifulSoup(req.text, 'html.parser')
-        htmlobj = soup.find(id='UltimoEvento')
-        textoobj = htmlobj.strong.text
-        dataobj = htmlobj.text.split()[-1]
+
+        htmlobjpostagem = soup.find(id='EventoPostagem')
+        htmlobjsaiuentrega = soup.find(id='UltimoEvento')
+        htmlobjprevisaoentrega = soup.find(id='DataEntrega')
+
+        textopostagem = htmlobjpostagem.strong.text
+        textosaiuentrega = htmlobjsaiuentrega.strong.text
+        textoprevisaoentrega = htmlobjprevisaoentrega.strong.text
+
+        datapostagem = htmlobjpostagem.text.split()[-1]
+        datasaiuentrega = htmlobjsaiuentrega.text.split()[-1]
+        dataprevisaoentrega = htmlobjprevisaoentrega.text.split()[-1]
+
 
         print('=-------------------------------------------------------------------------=>')
-        print(f'|\033[1m {i} \033[0m                                                           |')
-        print(f'| {textoobj} no dia {dataobj}               |')
+        print(f'\033[1m {i} \033[0m\n')
+        print(textopostagem)
+        print(datapostagem)
+
+        print(textosaiuentrega)
+        print(datasaiuentrega)
+
+        print(textoprevisaoentrega)
+        print(dataprevisaoentrega)
         print('<=-------------------------------------------------------------------------=')
+
+
+        # htmlobj = soup.find(id='EventoPostagem')
+        # textoobj = htmlobj.strong.text
+        # dataobj = htmlobj.text.split()[-1]
     except Exeption as e:
         print(e)
